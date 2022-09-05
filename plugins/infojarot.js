c6b95db4b469d75b0c7ac0d967ab7e7a9ad6375f
levi <111189476+ImleviOfc@users.noreply.github.com>
2022-09-05 13:27:59 +0800

    Update infojarot.js

--- plugins/infojarot.js
+++ plugins/infojarot.js
@@ -1,22 +1,33 @@
 let handler  = async (m, { conn, usedPrefix }) => { 
 conn.reply(m.chat, `
-╭─˗ˏˋ *INFORMASI OWNER* ´ˎ˗
-│ ✎ _Nama_ : Jarot
-│ ✎ _Sekolah_ : Lulus
-│ ✎ _Umur_ : 17
-│ ✎ _Asal_ : Jawa timur
-│ ✎ _Status_ : Turu deck
-│ ✎ _Official Grup 1_ :
-│    https://chat.whatsapp.com/IqZke0cAG6G3iVsnB9myfL
-│ ✎ _Official Grup 2_ :
-│    https://chat.whatsapp.com/HjRHck1G3WRHOx97fJkdMN
-│ ✎ _Youtube _:
-│    https://youtube.com/channel/UCW7iXlE7TgvJMIXQck4NYBQ
-│ ✎ _Instagram_ : 
-│    https://instagram.com/oscarbotz_
-│ ✎ _WhatsApp_ :
-│    wa.me/6285850539404
-╰───────────────────
+╭━━━━「 *SEWA* 」
+┊⫹⫺ *Hemat:* _3k/grup (1 minggu)_
+┊⫹⫺ *Normal:* _5k/grup (1 bulan)_
+┊⫹⫺ *Standar:* _10k/grup (2 bulan)_
+┊⫹⫺ *Pro:* _35k/grup (4 bulan)_                                                      
+┊⫹⫺ *Vip:* = _65k/grup (6 bulan & 1 bulan *Premium*)_
+╰═┅═━––––––๑
+
+╭━━━━「 *PREMIUM* 」
+┊⫹⫺ *Hemat:* _3k (1 minggu)_
+┊⫹⫺ *Normal:* _10k (1 bulan)_
+┊⫹⫺ *Pro:* _40k (4 bulan)_
+┊⫹⫺ *Vip:* _50k (8 bulan)_                                               
+┊⫹⫺ *Permanent:* = _999k (Unlimited)_
+╰═┅═━––––––๑
+
+*⫹⫺ PAYMENT:*
+• *Pulsa:* [085172446692]
+• *Dana:* [085172446692]
+
+–––––– *🐾 Kebijakan* ––––––
+🗣️: Kak, Kok harganya mahal banget?
+💬: Mau tawar menawar? boleh, silahkan chat owner aja
+
+🗣️: Scam ga nih kak?
+💬: Enggalah
+
+▌│█║▌║▌║║▌║▌║█│▌
 `.trim(), m)
 }
 
