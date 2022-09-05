733eefd7b94c19e04e89d61b25a1e771e72378ea
levi <111189476+ImleviOfc@users.noreply.github.com>
2022-09-05 13:08:40 +0800

    Update config.js

--- config.js
+++ config.js
@@ -31,17 +31,17 @@ global.APIKeys = { // APIKey Here
   'https://api.adiofficial.xyz': 'apikey lu'
 }
 
-//global.wm = '𝑴𝒚𝑩𝒐𝒕-𝑴𝒖𝒍𝒕𝒊 𝑫𝒆𝒗𝒊𝒄𝒆'
+//global.wm = 'ཌ✼ᴠᴇɴ-ʙᴏᴛᴢ✼ད'
 global.wait = '_*𝒔𝒆𝒅𝒂𝒏𝒈 𝒅𝒊 𝒑𝒓𝒐𝒔𝒆𝒔...*_'
 global.eror = '_*𝒎𝒂𝒂𝒇 𝒔𝒆𝒓𝒗𝒆𝒓 𝒆𝒓𝒓𝒐𝒓...*_'
 
 //========Url Template Buttons=========//
 global.dtu = '𝑌𝑜𝑢𝑡𝑢𝑏𝑒'
-global.urlnya = "https://youtube.com/channel/UCW7iXlE7TgvJMIXQck4NYBQ"
+global.urlnya = "https://youtube.com/channel/UC2GOH5mCArh3E4J3sGFMC5g"
 
 //============= callButtons =============//
 global.dtc = '𝒄𝒂𝒍𝒍 𝒐𝒘𝒏𝒆𝒓'
-global.phn = '62 857-1313-7903'
+global.phn = '+62 851-7244-6692'
 
 //============= Games ================//
 global.benar = '_*Benar*_'
