988b8c36780735963e22e575e107a8f57d994a0c
levi <111189476+ImleviOfc@users.noreply.github.com>
2022-09-05 13:17:39 +0800

    Update infoalpi.js

--- plugins/infoalpi.js
+++ plugins/infoalpi.js
@@ -1,13 +1,13 @@
 let handler  = async (m, { conn, usedPrefix }) => { 
 conn.reply(m.chat, `
-╭─˗ˏˋ *INFORMASI Alpi* ´ˎ˗
-│ ✎ _Nama_ : Alpi
-│ ✎ _Sekolah_ : SMA 1 BANTARBOLANG
-│ ✎ _Umur_ : 17
-│ ✎ _Asal_ : Jatim
-│ ✎ _Status_ : Pelajar SMA
+╭─˗ˏˋ *INFORMASI ཌ✼ʟᴇᴠɪ-ʏᴀɴ-xʜɪᴀᴏ✼ད* ´ˎ˗
+│ ✎ _Nama_ : Levi adnansyah
+│ ✎ _Sekolah_ : SMP 6 SAMBALIUNG
+│ ✎ _Umur_ : 14
+│ ✎ _Asal_ : KALTIM
+│ ✎ _Status_ : Pelajar SMP
 │ ✎ _WhatsApp_ :
-│    wa.me/6285713137903
+│    wa.me/6285172446692
 ╰──────────────
 `.trim(), m)
 }
